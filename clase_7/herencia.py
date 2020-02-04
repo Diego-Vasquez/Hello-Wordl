ef035e91b13cb1ba570944133f02fecf14125321
Diego-Vasquez <diego.vasquez.l@uni.pe>
2020-02-04 17:47:37 -0500

    v 1.0.6

---
+++ clase_7/herencia.py
@@ -0,0 +1,10 @@
+import random
+
+class Animal():
+    def __init__(self, spices='Unknown', name='Unknown',\
+        size='Unknown', onomatopoeia='Unknown',\
+        age='Unknown', gender='Unknown')
+        sefl.spices=spices
+        self.name=name
+        self.size=size
+        self.onomatopoeia=onomatopoeia
